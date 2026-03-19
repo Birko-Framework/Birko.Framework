e19733748d14e7fae12117614932a3eb1b412525
František Bereň <birko@live.com>
2026-03-19 19:11:27 +0100

    Add missing using directive for Birko.Configuration

--- Birko.Workflow.ElasticSearch/ElasticSearchWorkflowInstanceStore.cs
+++ Birko.Workflow.ElasticSearch/ElasticSearchWorkflowInstanceStore.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Birko.Data.ElasticSearch.Stores;
 using Birko.Data.Stores;
+using Birko.Configuration;
 using Birko.Workflow.Core;
 using Birko.Workflow.ElasticSearch.Models;
 using Birko.Workflow.Execution;
