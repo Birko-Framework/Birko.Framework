f1b9837e86046ab6885710a68306fad0d11e8d6c
František Bereň <birko@live.com>
2026-03-23 11:19:00 +0100

    refactor: simplify ProductPartnerCode class by removing database attributes

--- Birko.Models.Product/Models/ProductPartnerCode.cs
+++ Birko.Models.Product/Models/ProductPartnerCode.cs
@@ -1,18 +1,14 @@
 using System;
-using Birko.Data.SQL.Attributes;
 using Birko.Data.Models;
 
 namespace Birko.Models.Product
 {
-    [Table("ProductPartnerCodes")]
-    public class ProductPartnerCode : AbstractDatabaseLogModel, ILoadable<ViewModels.ProductPartnerCode>
+    public class ProductPartnerCode : AbstractLogModel, ILoadable<ViewModels.ProductPartnerCode>
     {
         public Guid? ProductGuid { get; set; }
 
-        [PrecisionField(256)]
         public string PartnerName { get; set; } = null!;
 
-        [PrecisionField(256)]
         public string Code { get; set; } = null!;
 
         public virtual void LoadFrom(ViewModels.ProductPartnerCode data)
